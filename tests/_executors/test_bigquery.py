2634cfc509ed6f0db0bc75af9c6077364ca75a1d
Jasper Ginn <jasperginn@gmail.com>
2024-09-07 09:08:22 +0200

    test: adding executor tests

---
+++ tests/_executors/test_bigquery.py
@@ -0,0 +1,155 @@
+from typing import Iterator
+from unittest import mock
+
+import pytest
+import sqlalchemy_bigquery
+from devtools import run_container
+from google.api_core.client_options import ClientOptions
+from google.auth.credentials import AnonymousCredentials
+from google.cloud import bigquery
+from manuel._config import BigQuerySqlConfig
+from manuel._executors import BigQuerySqlExecutor
+from sqlalchemy import Engine, NullPool, create_engine
+from sqlalchemy.orm import Session
+
+PROJECT_ID = "test-project"
+DATASET_NAME = "dataset"
+LOCATION = "europe-west4"
+
+
+@pytest.fixture(scope="module")
+def connection_string() -> str:
+    return "bigquery://%s&location=%s?user_supplied_client=True" % (
+        PROJECT_ID,
+        LOCATION,
+    )
+
+
+@pytest.fixture(scope="module")
+def client() -> bigquery.Client:
+    return bigquery.Client(
+        project=PROJECT_ID,
+        client_options=ClientOptions(api_endpoint="http://0.0.0.0:9050"),
+        credentials=AnonymousCredentials(),
+    )
+
+
+@pytest.fixture
+def bigquery_sql_executor(client: bigquery.Client) -> BigQuerySqlExecutor:
+    return BigQuerySqlExecutor(engine_connect_args={"client": client})
+
+
+@pytest.fixture
+def config():
+    return BigQuerySqlConfig(
+        project=PROJECT_ID, location=LOCATION, user_supplied_client=True
+    )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def bigquery_container():
+    with run_container(
+        image="ghcr.io/goccy/bigquery-emulator:0.6.5",
+        command=["--project", PROJECT_ID, "--dataset", DATASET_NAME],
+        image_name_prefix="manuel-bigquery",
+        platform="linux/amd64",
+        ports={
+            "9050/tcp": ("127.0.0.1", 9050),  # API
+            "9060/tcp": ("127.0.0.1", 9060),  # GRPC
+        },
+        wait_seconds=1,
+    ) as container:
+        yield container
+
+
+@pytest.fixture(scope="module")
+def engine(
+    bigquery_container, connection_string: str, client: bigquery.Client
+) -> Iterator[Engine]:
+    engine = create_engine(
+        connection_string, poolclass=NullPool, connect_args={"client": client}
+    )
+    yield engine
+    engine.dispose()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def session(engine: Engine) -> Iterator[Session]:
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(scope="session")
+def statement() -> str:
+    return f"""
+    CREATE TABLE `{PROJECT_ID}.{DATASET_NAME}.table` (
+        `ADDRESS_ID` STRING,
+        `INDIVIDUAL_ID` STRING,
+        `FIRST_NAME` STRING,
+        `LAST_NAME` STRING
+    );
+    """
+
+
+@pytest.mark.parametrize(
+    "config",
+    [
+        BigQuerySqlConfig(
+            project=PROJECT_ID, location=LOCATION, user_supplied_client=False
+        ),
+        BigQuerySqlConfig(
+            project=PROJECT_ID, location=LOCATION, user_supplied_client=True
+        ),
+    ],
+)
+def test_bigquery_sql_executor_format_connection_string(
+    bigquery_sql_executor: BigQuerySqlExecutor, config: BigQuerySqlConfig
+):
+    connection_string = bigquery_sql_executor.format_connection_string(
+        **config.model_dump()
+    )
+    assert (
+        connection_string
+        == "bigquery://test-project&location=europe-west4?user_supplied_client=True"
+        if config.user_supplied_client
+        else "bigquery://test-project&location=europe-west4"
+    )
+
+
+def test_bigquery_sql_executor_get_engine(bigquery_sql_executor: BigQuerySqlExecutor):
+    with bigquery_sql_executor.get_engine(
+        connection_string="bigquery://test-project&location=europe-west4?user_supplied_client=True"
+    ) as engine:
+        assert engine.driver == "bigquery"
+        assert isinstance(engine.dialect, sqlalchemy_bigquery.BigQueryDialect)
+
+
+def test_bigquery_sql_executor_execute_sql(
+    statement: str, bigquery_sql_executor: BigQuerySqlExecutor, session: Session
+):
+    bigquery_sql_executor.execute_sql(sql=statement, session=session)
+    session.rollback()
+
+
+@mock.patch("manuel._executors.base.Session")
+def test_bigquery_sql_executor_run(
+    mock_session: mock.MagicMock,
+    statement: str,
+    bigquery_sql_executor: BigQuerySqlExecutor,
+    config: BigQuerySqlConfig,
+):
+    get_engine_mock = mock.MagicMock()
+    execute_sql_mock = mock.MagicMock()
+    sqlalchemy_session_mock = mock.MagicMock()
+    # Add a mock to the return value of the mocked Session context manager return value
+    mock_session.return_value.__enter__.return_value = sqlalchemy_session_mock
+    # Add mocks to these two methods, which are called in the run method
+    # We have checked them above, so all we care about is that the arguments are
+    # passed correctly.
+    bigquery_sql_executor.get_engine = get_engine_mock
+    bigquery_sql_executor.execute_sql = execute_sql_mock
+    bigquery_sql_executor.run(sql=statement, **config.model_dump())
+    get_engine_mock.assert_called_once_with(
+        "bigquery://test-project&location=europe-west4?user_supplied_client=True"
+    )
+    execute_sql_mock.assert_called_once_with(statement, sqlalchemy_session_mock)
---
+++ tests/_executors/test_postgres.py
@@ -0,0 +1,148 @@
+from typing import Iterator
+from unittest import mock
+
+import pytest
+from devtools import run_container
+from manuel._config import PostgresSqlConfig
+from manuel._executors.postgres import PostgresSqlExecutor
+from pytest_postgresql.janitor import DatabaseJanitor
+from sqlalchemy import Engine, NullPool, create_engine
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import Session
+
+IMAGE = "postgres:15.1"
+POSTGRES_PASSWORD = "postgres"
+POSTGRES_USER = "postgres"
+POSTGRES_PORT = 65432
+POSTGRES_HOST = "127.0.0.1"
+POSTGRES_DATABASE = "test_db"
+
+
+@pytest.fixture(scope="module")
+def connection_string() -> str:
+    return "postgresql+psycopg://%s:%s@%s:%s/%s" % (
+        POSTGRES_USER,
+        POSTGRES_PASSWORD,
+        POSTGRES_HOST,
+        POSTGRES_PORT,
+        POSTGRES_DATABASE,
+    )
+
+
+@pytest.fixture
+def postgres_sql_executor() -> PostgresSqlExecutor:
+    return PostgresSqlExecutor()
+
+
+@pytest.fixture
+def config() -> PostgresSqlConfig:
+    return PostgresSqlConfig(
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        database=POSTGRES_DATABASE,
+    )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def postgres_container():
+    with run_container(
+        image=IMAGE,
+        image_name_prefix="manuel-postgres",
+        environment=[f"POSTGRES_PASSWORD={POSTGRES_PASSWORD}"],
+        ports={"5432/tcp": (POSTGRES_HOST, POSTGRES_PORT)},
+        wait_seconds=1,
+    ) as container:
+        yield container
+
+
+@pytest.fixture(scope="module")
+def engine(postgres_container, connection_string: str) -> Iterator[Engine]:
+    with DatabaseJanitor(
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+        dbname=POSTGRES_DATABASE,
+        version=15,
+    ):
+        engine = create_engine(
+            connection_string,
+            poolclass=NullPool,
+        )
+        yield engine
+        engine.dispose()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def session(engine: Engine) -> Iterator[Session]:
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(scope="module")
+def statement() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS test_table (
+        id SERIAL PRIMARY KEY,
+        age INT,
+        category VARCHAR(10)
+    );
+
+    INSERT INTO test_table (age, category)
+    VALUES (25, 'A');
+    """
+
+
+def test_postgres_sql_executor_format_connection_string(
+    postgres_sql_executor: PostgresSqlExecutor, config: PostgresSqlConfig
+):
+    connection_string = postgres_sql_executor.format_connection_string(
+        **config.model_dump()
+    )
+    assert (
+        connection_string
+        == "postgresql+psycopg://postgres:**********@127.0.0.1:65432/test_db"  # pydantic masks secret string
+    )
+
+
+def test_postgres_sql_executor_get_engine(
+    postgres_sql_executor: PostgresSqlExecutor, connection_string: str
+):
+    with postgres_sql_executor.get_engine(
+        connection_string=connection_string
+    ) as engine:
+        assert engine.driver == "psycopg"
+        assert isinstance(engine.dialect, postgresql.psycopg.PGDialect_psycopg)
+
+
+def test_postgres_sql_executor_execute_sql(
+    statement: str, postgres_sql_executor: PostgresSqlExecutor, session: Session
+):
+    postgres_sql_executor.execute_sql(sql=statement, session=session)
+    session.rollback()
+
+
+@mock.patch("manuel._executors.base.Session")
+def test_postgres_sql_executor_run(
+    mock_session: mock.MagicMock,
+    statement: str,
+    postgres_sql_executor: PostgresSqlExecutor,
+    config: PostgresSqlConfig,
+):
+    get_engine_mock = mock.MagicMock()
+    execute_sql_mock = mock.MagicMock()
+    sqlalchemy_session_mock = mock.MagicMock()
+    # Add a mock to the return value of the mocked Session context manager return value
+    mock_session.return_value.__enter__.return_value = sqlalchemy_session_mock
+    # Add mocks to these two methods, which are called in the run method
+    # We have checked them above, so all we care about is that the arguments are
+    # passed correctly.
+    postgres_sql_executor.get_engine = get_engine_mock
+    postgres_sql_executor.execute_sql = execute_sql_mock
+    postgres_sql_executor.run(sql=statement, **config.model_dump())
+    get_engine_mock.assert_called_once_with(
+        "postgresql+psycopg://postgres:**********@127.0.0.1:65432/test_db"
+    )
+    execute_sql_mock.assert_called_once_with(statement, sqlalchemy_session_mock)
